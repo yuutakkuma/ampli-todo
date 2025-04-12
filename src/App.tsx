32aa05496a784fc5671de203a72ce28e3caa7c06
Yuta Watanabe <yuutakkuma.74@gmail.com>
2025-04-13 05:06:01 +0900

    Add biome formater.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button onClick={() => {
-       alert("on click! shadcn button!");
-      }}>Click me</Button>
-    </div>
-  )
+	return (
+		<div className="flex flex-col items-center justify-center min-h-svh">
+			<Button
+				onClick={() => {
+					alert("on click! shadcn button!");
+				}}
+			>
+				Click me
+			</Button>
+		</div>
+	);
 }
 
-export default App
+export default App;
